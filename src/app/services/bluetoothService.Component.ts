2a192cf807a31ea71c998fcf91dc999392b92fbc
Marcello Rosati <marcellorosati1095@gmail.com>
2020-04-19 20:07:27 +0200

    updated bluetooth

--- src/app/services/bluetoothService.Component.ts
+++ src/app/services/bluetoothService.Component.ts
@@ -2,45 +2,104 @@ import { Component } from '@angular/core';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { IDevice } from 'src/interface/IDevice';
 
+
+
 @Component({
     template: ''
 })
-export class BluetoothServiceComponent {
+export class  BluetoothServiceComponent {
 
   constructor(private bluetoothSerial: BluetoothSerial) { }
 
-  public isEnabled(): Promise<boolean> {
+  public  async isEnabled(): Promise<boolean> {
     let value = false;
     const promise = new Promise<boolean>(resolve => {
-      setTimeout(() => {
-        resolve(value);
-      }, 1000);
+      setTimeout(()=>{
+resolve(value)
+      })
+    
     });
-    this.bluetoothSerial.isEnabled()
-    .then(enabled => {
-      if (enabled === 'OK') {
-        value = true;
-        return promise;
-      }
-    });
+    await Promise.resolve(this.bluetoothSerial.isEnabled()
+      .then(enabled => {
+        if (this.convertResponseBluetooth(enabled)) {
+          value = true;
+        }
+      }) 
+      .catch(err => {
+        // TODO: gestire l'errore
+        console.log(err, 'notenabled');
+      })
+    )
     return promise;
   }
 
-  public searchDevices(): IDevice[] {
+  public async searchDevices(): Promise<IDevice[]> {
     const myDevices: IDevice[] = [];
-
-    this.bluetoothSerial.discoverUnpaired()
-    .then((devices: IDevice[]) => {
-      devices.forEach((device: IDevice) => {
-        if ( device.Address !== undefined && device.Name !== undefined) {
-          myDevices.push(device);
-        }
-      });
-    })
-    .catch(err => {
-      // TODO: gestire l'errore
-      console.log(err, 'devices');
+    const promise = new Promise<IDevice[]>(resolve => {
+      resolve(myDevices);
     });
-    return myDevices;
+
+    await Promise.resolve(
+      this.bluetoothSerial.discoverUnpaired()
+      .then((devices: IDevice[]) => {
+        devices.forEach((device: IDevice) => {
+          if ( device.address !== undefined && device.name !== undefined) {
+            myDevices.push(device);
+          }
+        });
+      })
+      .catch(err => {
+        // TODO: gestire l'errore
+        console.log(err, 'devices');
+      })
+    )
+    return promise;
+  }
+
+ 
+  public connectToDevice(macAddress : string): boolean{
+    let response=false;
+    this.bluetoothSerial.connect(macAddress)
+    .subscribe((res) => {
+    if(this.convertResponseBluetooth(res))
+      response=true;
+      console.log("isConnected",response)
+    },(err) => {
+      console.log(err,"errore")
+    }) 
+    return response;  
+  }
+
+  public showBluetoothSettings(){
+    this.bluetoothSerial.showBluetoothSettings()
+    .then((settings)=>{
+      console.log(settings,"settingBluetooth")
+    })
+    .catch(err=>
+      console.log(err,"errorsetting"))
+  }
+
+  public disconnectBluetooth():boolean{
+    let response=false;
+    this.bluetoothSerial.disconnect()
+    .then((res)=>{
+     if(this.convertResponseBluetooth(res))
+     response=true
+     console.log("Disconnect",response)
+    })
+    return response
+  }
+public sendMessageToBluetooth(msg:string)
+{
+  this.bluetoothSerial.write(msg)
+  .then((res)=>{
+    console.log(res)
+   })
+  
+}
+  private convertResponseBluetooth(response: string): boolean{
+    if(response === 'OK'|| response ==='ok')
+      return true;
+      return false;
   }
 }

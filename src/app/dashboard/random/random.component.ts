c3527558348fdbf70061f6db34bbb41f688fef28
Marcello Rosati <marcellorosati1095@gmail.com>
2020-05-31 23:00:07 +0200

    funzione SEND DICE!

--- src/app/dashboard/random/random.component.ts
+++ src/app/dashboard/random/random.component.ts
@@ -11,11 +11,12 @@ import { CommonServiceComponent } from '../../services/commonService.Component';
   styleUrls: ['./random.component.scss'],
 })
 export class RandomComponent implements OnInit {
-public message:string ="1,6"
+public message:string ="1,6;"
 nameFile = 'typeDices';
 public showInputQuantity :boolean =false;
 public dicesSelected:ITypeDice[] = [];
 public typeDices: ITypeDice[];
+
   constructor(
     public modalService: ModalServiceComponent,
     public bluetoothService: BluetoothServiceComponent,
@@ -26,31 +27,59 @@ public typeDices: ITypeDice[];
     this.commonSerivce.readJson(this.nameFile).then(dices => {
       this.typeDices = dices;
     });
-   // this.bluetoothService.sendMessageToBluetooth(this.message)
+   this.bluetoothService.sendMessageToBluetooth(this.message)
   }
 
  async onSelected(value){
 
   await value.forEach(element => {
     let x = this.typeDices.find(f => f.value == element);
-    console.log(x)
+   
     this.dicesSelected.push(x);
     
   });
-console.log(this.dicesSelected)
+
   if(this.dicesSelected.length > 0){
     this.showInputQuantity = true;
   }
 
 
 }
-async onChange(value:string,dices:ITypeDice){
+async onChange(value:string,dices){
+
   this.dicesSelected.find(f => f.value == dices.value).quantity =parseInt(value);
-console.log(this.dicesSelected)
+
+
+
 }
 
 onSubmit(dices){
+  let sommaTotale=0
+ console.log(dices)
+ console.log(dices.length)
+ 
+  for(let x=0;x<dices.length;x++){
+ 
+   sommaTotale=dices[x].value*dices[x].quantity +sommaTotale 
+
+    console.log(sommaTotale)
+  }
+  if(sommaTotale>999)
+  {
+    console.log("troppo grande uagliu")
+  }
+  else{
+    for(let x=0;x<dices.length;x++){
+ 
+     this.message =dices[x].value+","+dices[x].quantity+";"
+   
+       console.log(sommaTotale)
+     }
+  
+  }
+
+  }
   //che se la moltiplicazione tra quantita e valore del dado  sommati danno + di 999 deve cambiare
 
-}
+
 }
